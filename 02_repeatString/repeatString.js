2f0fa79c64dff65743d9c21fd674326594d4e479
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-03-30 02:38:20 -0500

    Add string to returnString every loop then return it

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,9 @@
 const repeatString = function(string, number) {
     let returnString = "";
     for(let i = 0; i < number; i++) {
-
+        returnString += string;
     }
+    return returnString;
 };
 
 // Do not edit below this line

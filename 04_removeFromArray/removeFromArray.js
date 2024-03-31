0c5bcc11b670102c52c20fc3ebfe9d35a747d5bf
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-03-30 23:54:01 -0500

    Add algorithm comment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function() {
-
+    // Create new array
+    // Start loop
+    // if element at loop index is argument
+    // continue
+    // else add element at loop index to array
+    // End loop
+    // Return array
 };
 
 // Do not edit below this line

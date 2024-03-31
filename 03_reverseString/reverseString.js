fd7714179b80187b2ad130b9cdabcab4b082b98c
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-03-30 23:05:21 -0500

    Comment the algorithm

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
 const reverseString = function(string) {
-
+    // Store string length into variable
+    // Create 'reverseString array with size of string length
+    // Create reversed string variable starting index at last element of string
+    // Start Loop
+    // Get character at loop index of string
+    // and put it at reverseString index of the reversed string array
+    // Decrement reverse string array index
+    // End Loop
+    // Return reversed string array as string
 };
 
 // Do not edit below this line
